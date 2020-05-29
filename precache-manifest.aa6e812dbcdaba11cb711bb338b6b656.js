2a9eadb686e55f622f013f1688be64a8c18d15d2
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-05-29 15:10:35 +0530

    Updates

---
+++ precache-manifest.aa6e812dbcdaba11cb711bb338b6b656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d1af74e4e2101e8deb60866ebd5277",
+    "url": "/internship-demo/index.html"
+  },
+  {
+    "revision": "97e402f9b7b72b7e5b53",
+    "url": "/internship-demo/static/css/main.c23e13c2.chunk.css"
+  },
+  {
+    "revision": "c8a745d850573977d2b5",
+    "url": "/internship-demo/static/js/2.20259571.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/internship-demo/static/js/2.20259571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e402f9b7b72b7e5b53",
+    "url": "/internship-demo/static/js/main.ad28c528.chunk.js"
+  },
+  {
+    "revision": "91bfd6a58a501015d1af",
+    "url": "/internship-demo/static/js/runtime-main.bdc3e780.js"
+  }
+]);
